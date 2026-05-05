dead5be2cb15bd28eab6163bd20e51a89b8490e3
wlrnjs <wlrnjs5824@gmail.com>
2026-05-05 23:20:18 +0900

    rename(component): CheckBoxButton export 수정

--- apps/monitor-web/src/components/common/buttons/index.ts
+++ apps/monitor-web/src/components/common/buttons/index.ts
@@ -1,4 +1,4 @@
 export { default as BasicButton } from "./BasicButton";
-export { default as Checkbox } from "./CheckboxButton";
+export { default as CheckboxButton } from "./CheckboxButton";
 export { default as ClearButton } from "./ClearButton";
 export { default as IconButton } from "./IconButton";
