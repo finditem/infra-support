f2fe5e31bea3f63145d715cb68d2085e2d473a5d
wlrnjs <wlrnjs5824@gmail.com>
2026-05-06 21:18:13 +0900

    design(login): flex-center 변경

--- apps/monitor-web/src/pages/Login/Login.tsx
+++ apps/monitor-web/src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm, LoginHeader } from "./_components";
 
 const Login = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="min-h-screen flex-center">
       <section
         aria-label="찾아줘! API 모니터링 로그인"
         className="w-full max-w-[720px] gap-10 rounded-xl border border-[#DFDFDF] bg-white p-[100px] flex-col-center"
