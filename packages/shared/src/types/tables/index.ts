8abe5c5297c5e1180c974869214b7f7df2a1670b
junye0l <junyeolkim00@gmail.com>
2026-05-11 20:32:55 +0900

    feat(manual_test_results): manual_test_results 테이블 타입 구조 작성

--- packages/shared/src/types/tables/index.ts
+++ packages/shared/src/types/tables/index.ts
@@ -1,6 +1,11 @@
 export type { ApisInsert, ApisRow, ApisUpdate } from "./apis";
 export type { ErrorLogsInsert, ErrorLogsRow, ErrorLogsUpdate } from "./error_logs";
 export type {
+  ManualTestResultsInsert,
+  ManualTestResultsRow,
+  ManualTestResultsUpdate,
+} from "./manual_test_results";
+export type {
   MonitoringResultsInsert,
   MonitoringResultsRow,
   MonitoringResultsUpdate,
