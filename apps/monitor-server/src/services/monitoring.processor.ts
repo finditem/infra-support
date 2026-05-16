1bb6e3e009b77dda53eea7d51bd7964571e9e222
junye0l <junyeolkim00@gmail.com>
2026-05-17 04:56:39 +0900

    refactor(vworld): UA 구조 수정

--- apps/monitor-server/src/services/monitoring.processor.ts
+++ apps/monitor-server/src/services/monitoring.processor.ts
@@ -66,7 +66,7 @@ const buildHeaders = (rawUrl: string): HeadersInit => {
 
   if (url.hostname === "api.vworld.kr") {
     headers["User-Agent"] = 
-      "Mozilla/5.0 (Windows NT 10.0; Win64; 64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36";
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36";
     headers.Referer = "https://www.finditem.kr";
   }
 
