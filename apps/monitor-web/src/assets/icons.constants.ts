e85e36a99fa22981e9e6bb7c372f51cb9f66902d
junye0l <junyeolkim00@gmail.com>
2026-05-23 04:30:32 +0900

    feat(errorLog): 에러로그 헤더 기본구조 작성

--- apps/monitor-web/src/assets/icons.constants.ts
+++ apps/monitor-web/src/assets/icons.constants.ts
@@ -4,6 +4,7 @@ import Check from "@/assets/icons/check.svg?react";
 import Clear from "@/assets/icons/clear.svg?react";
 import EditPencil from "@/assets/icons/edit-pencil.svg?react";
 import LoadingSpinner from "@/assets/icons/loading-spinner.svg?react";
+import ErrorLog from "@/assets/icons/errorLog.svg?react";
 
 export const ICON_MAP = {
   alert: AlertIcon,
@@ -12,4 +13,5 @@ export const ICON_MAP = {
   clear: Clear,
   editPencil: EditPencil,
   loadingSpinner: LoadingSpinner,
+  errorLog: ErrorLog,
 } as const;
---
+++ apps/monitor-web/src/pages/ErrorLog/_components/LogHeader.tsx
@@ -0,0 +1,12 @@
+import { Icon } from "@/components";
+
+const LogHeader = () => {
+  return (
+    <section>
+      <Icon height={26} name="errorLog" width={24} />
+      <h1>장애/에러 로그</h1>
+    </section>
+  );
+};
+
+export default LogHeader;
