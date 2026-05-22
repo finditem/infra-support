8a9c6bbbf6a597cb0d42d1bd265076afafef1f72
junye0l <junyeolkim00@gmail.com>
2026-05-23 04:49:48 +0900

    feat(log-header): 스타일링 수정 및 접근성 추가

--- apps/monitor-web/src/pages/ErrorLog/ErrorLog.tsx
+++ apps/monitor-web/src/pages/ErrorLog/ErrorLog.tsx
@@ -1,7 +1,9 @@
+import { LogHeader } from "./_components";
+
 const ErrorLog = () => {
   return (
     <div>
-      <h1>장애/에러 로그 페이지</h1>
+      <LogHeader />
     </div>
   );
 };
--- apps/monitor-web/src/pages/ErrorLog/_components/LogHeader.tsx
+++ apps/monitor-web/src/pages/ErrorLog/_components/LogHeader.tsx
@@ -2,10 +2,15 @@ import { Icon } from "@/components";
 
 const LogHeader = () => {
   return (
-    <section>
-      <Icon height={26} name="errorLog" width={24} />
-      <h1>장애/에러 로그</h1>
-    </section>
+    <header
+      aria-labelledby="error-log-title"
+      className="-mx-8 -mt-8 flex items-center border border-[#E2E8F0] bg-white p-10"
+    >
+      <Icon className="m-4" height={26} name="errorLog" width={24} />
+      <h1 id="error-log-title" className="text-header1-bold">
+        장애/에러 로그
+      </h1>
+    </header>
   );
 };
 
