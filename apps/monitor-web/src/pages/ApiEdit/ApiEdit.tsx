9f61d96d5af125cfdee2328f47b47fea373387dc
junye0l <junyeolkim00@gmail.com>
2026-08-05 07:15:29 +0900

    design(api-edit-header): API 수정 헤더 퍼블리싱 작업

--- apps/monitor-web/src/pages/ApiEdit/ApiEdit.tsx
+++ apps/monitor-web/src/pages/ApiEdit/ApiEdit.tsx
@@ -1,11 +1,9 @@
-import { useParams } from "react-router-dom";
+import { ApiEditHeader } from "./_components";
 
 const ApiEdit = () => {
-  const { apiId } = useParams<{ apiId: string }>();
-
   return (
     <div>
-      <h1>API 수정 - {apiId}</h1>
+      <ApiEditHeader />
     </div>
   );
 };
