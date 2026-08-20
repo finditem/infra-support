73f1e3bd310daf29251a9d53af495e56e31471f7
suhyeon0111 <soo4833@naver.com>
2026-08-20 11:06:08 +0900

    feat(schedule): 앱 메타 설명 문구를 찾길로 변경

--- apps/schedule/src/app/layout.tsx
+++ apps/schedule/src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "찾길 팀 일정",
-  description: "찾아줘! 팀 내부 일정 관리 툴",
+  description: "찾길 팀 내부 일정 관리 툴",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
